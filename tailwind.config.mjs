579c0837e6a559539fb525a0b440374515531940
Serge <93369654+comoestassergio@users.noreply.github.com>
2024-10-05 10:34:08 +0300

    feat: create two main pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        ultra: "Ultra, sans-serif",
+        roboto: "Roboto, sans-serif",
+      },
+      borderRadius: {
+        custom: "20px",
+        "custom-sm": "10px",
+      },
+    },
+    fontFamily: {
+      sans: "Roboto, sans-serif",
+    },
+  },
+  plugins: [],
+};
